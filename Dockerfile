1e8720b3259e3bd9fbb30c10fcb933ec472ca95e
Ty Eggen <tycho.eggen@gmail.com>
2019-04-03 00:47:12 -0700

    Fixes

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,4 @@ FROM nodered/node-red-docker
 ENTRYPOINT ["/bootstrap/scripts/docker_entrypoint.sh"]
 CMD ["npm", "start", "--", "--userDir", "/data"]
 
-VOLUME /bootstrap/etc
-
 COPY . /bootstrap
--- scripts/docker_entrypoint.sh
+++ scripts/docker_entrypoint.sh
@@ -2,21 +2,27 @@
 
 cd /data
 
-echo "Caching installed packages..."
-npm list --depth=0 | egrep -v $(pwd) | awk '{ print $2 }' | cut -f1 -d'@' | egrep -v '^$' > /bootstrap/data/packages-installed.txt
+if [ -f package.json ] ; then
+ echo "Caching installed packages..."
+ npm list --depth=0 | egrep -v $(pwd) | awk '{ print $2 }' | cut -f1 -d'@' | egrep -v '^$' > /bootstrap/cache/packages-installed.txt
+ 
+ echo "Installing missing packages..."
+ cat /bootstrap/conf/packages.txt | egrep -v '^(#|;|$)' | egrep -v -f /bootstrap/cache/packages-installed.txt | while read line node_module
+ do
+  npm install "$node_module"
+  res=$?
+  if [ $res -eq 0 ] ; then
+   echo "$node_module" >> /bootstrap/cache/packages-installed.txt
+  else
+   echo "Failed to install node_module: $node_module"
+   echo "Please fix the issue and restart the container."
+   exit
+  fi
+ done
+else
+ echo "Skipping packages install"
+fi
 
-echo "Installing missing packages..."
-cat /bootstrap/data/packages.txt | egrep -v '^(#|;|$)' | egrep -v -f /bootstrap/data/packages-installed.txt | while read line node_module
-do
- npm install "$node_module"
- res=$?
- if [ $res -eq 0 ] ; then
-  echo "$node_module" >> /bootstrap/data/packages-installed.txt
- else
-  echo "Failed to install node_module: $node_module"
-  echo "Please fix the issue and restart the container."
-  exit
- fi
-done
+cd /usr/src/node-red
 
 exec "$@"
